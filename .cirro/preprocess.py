300b010e087eab89599a53e4b13e575f26d6b0ef
Sam Minot <sminot@gmail.com>
2025-07-23 14:10:28 -0700

    Filter to only use BAM files from the input dataset

--- .cirro/preprocess.py
+++ .cirro/preprocess.py
@@ -6,9 +6,12 @@ from cirro.helpers.preprocess_dataset import PreprocessDataset
 def main():
     ds = PreprocessDataset.from_running()
 
+    # Only use the files from the list which are BAM files
+    samplesheet = ds.files[ds.files["file"].str.endswith(".bam")].copy()
+
     # Write out the list of files to process
-    ds.logger.info(ds.files.to_csv(index=None))
-    ds.files.to_csv("samplesheet.csv", index=None)
+    ds.logger.info(samplesheet)
+    samplesheet.to_csv("samplesheet.csv", index=None)
     ds.add_param("samplesheet", "samplesheet.csv")
 
 
